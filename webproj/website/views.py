1133e5524fbbb581bba22232921a722aec5a59af
psripac <psriprac@yahoo.com>
2020-05-25 22:03:23 -0700

    import scrollbar style

--- webproj/website/views.py
+++ webproj/website/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Welcome to <strong>Django</strong> with <em>Boostrap!</em>')
+    return render(request, 'index.html', {})
