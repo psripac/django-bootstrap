a323743462bf4075b0507f7bb403e6ef8944ef64
psripac <psriprac@yahoo.com>
2020-05-25 23:38:42 -0700

    secretkey changed

--- webproj/webproj/settings.py
+++ webproj/webproj/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n!dk8=0)zy(u0x=ae45y$jr201oyzw!36@(fzc6n@z645r_5a='
+with open('D:/secretkeys/django-bootstrap/secretkey.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
